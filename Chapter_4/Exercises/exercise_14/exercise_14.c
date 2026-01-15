82a1d4b67059fc506776cc705f46a15b5b115f36
bwrnardo <bernardobrtz1@gmail.com>
2026-01-15 09:46:36 -0300

    Commented exercise 14

--- Chapter_4/Exercises/exercise_14/exercise_14.c
+++ Chapter_4/Exercises/exercise_14/exercise_14.c
@@ -1,16 +1,14 @@
-#include <stdio.h>
-
-int main(void)
-{
-    // a * b - c * d + e
+/*   
+    a * b - c * d + e
     ((a * b) - (c * d)) + e;
 
-    // a / b % c / d
+    a / b % c / d
     ((a / b) % c) / d;
 
-    // - a - b + c - + d
+    - a - b + c - + d
     (((-a) - b) + c) - (+d);
 
-    // a * - b / c - d
+    a * - b / c - d
     ((a * (-b)) / c) - d;
-}d
+
+*/
